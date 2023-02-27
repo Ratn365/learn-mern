75e4e34e74d33c9182e576b868b3790b9beb0a4c
Ratn365 <Ratnesh.visionias@outlook.com>
2023-02-27 18:43:34 +0530

    2.4 routes and routeHandlers

--- app.js
+++ app.js
@@ -5,34 +5,42 @@ const app = express();
 
 app.use(express.json()); //for post request req.body
 
-/*******************routes**************************/
-app.get('/api/v1/tours', (req, res) =>
+/*******************routeHandler**************************/
+
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: { message: 'Hello World!', route: 'from route' },
-  })
-);
-app.post('/api/v1/tours', (req, res) => {
-  res.status(201).json({ status: 'success', data: { tour: req.body } });
-});
+  });
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const createTour = (req, res) => {
+  res.status(201).json({ status: 'success', data: { tour: req.body } });
+};
+const getTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     idParam: req.params.id,
     data: { tour: req.body },
   });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const { id, name, description } = req.body;
   res.status(200).json({ status: 'success', data: { id, name, description } });
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   const { id } = req.params;
   res.status(204).json({ status: 'success', data: null, id: id });
-});
+};
+/*******************routes**************************/
+app.get('/api/v1/tours', getAllTours);
+app.post('/api/v1/tours', createTour);
+
+app.get('/api/v1/tours/:id', getTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
 /******************server********************************/
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
