006c96287090750ef1bcaf5dadffe6ce7cdda4bb
Ratn365 <Ratnesh.visionias@outlook.com>
2023-02-27 17:49:30 +0530

    1. Express Server Setup

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello Worl');
+const express = require('express');
+const app = express();
+/*****************App imports****************************/
+//routes
+app.get('/', (req, res) => res.send('Hello World!'));
+
+//server
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
