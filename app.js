51220bd3e110b3172a5dc5b85b81c90845f9dd1c
Ratn365 <Ratnesh.visionias@outlook.com>
2023-02-27 18:29:50 +0530

    2.3 routes with relevent statuscode

--- app.js
+++ app.js
@@ -7,12 +7,28 @@ app.use(express.json()); //for post request req.body
 
 /*******************routes**************************/
 app.get('/api/v1/tours', (req, res) =>
-  res.json({ message: 'Hello World!', route: 'from route' })
+  res.status(200).json({
+    status: 'success',
+    data: { message: 'Hello World!', route: 'from route' },
+  })
 );
 app.post('/api/v1/tours', (req, res) => {
-  res.send(req.body);
+  res.status(201).json({ status: 'success', data: { tour: req.body } });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  res.status(200).send(req.params.id);
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const { id, name, description } = req.body;
+  res.status(200).send(`Name ${id} ${name}, desc ${description}`);
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(204).send(`Delete record with id ${id}`);
+});
 /******************server********************************/
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
