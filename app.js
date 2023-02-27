1c6a65101d7db68ab5d9e455a3070a5ec84fd01d
Ratn365 <Ratnesh.visionIas@outlook.com>
2023-02-27 23:54:48 +0530

    4. using 3rdd party middleware

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /*****************App imports****************************/
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 /*****************MiddleWare*****************************/
 
 app.use(express.json()); //for post request req.body
-
+app.use(morgan('dev'));
 /*******************routes**************************/
 app.use('/api/v1/tours', tourRouter);
 
-/******************server********************************/
-const port = 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//export app
+module.exports = app;
