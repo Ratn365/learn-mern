954f451d02ae2ab8bafa8dac89bb91ec5c658c46
Ratn365 <Ratnesh.visionias@outlook.com>
2023-02-27 19:04:37 +0530

    2.6 3rd way of routing modular structure

--- app.js
+++ app.js
@@ -1,47 +1,14 @@
 /*****************App imports****************************/
 const express = require('express');
 const app = express();
+const tourRouter = require('./routes/tourRouter');
 /*****************MiddleWare*****************************/
 
 app.use(express.json()); //for post request req.body
 
-/*******************routeHandler**************************/
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: { message: 'Hello World!', route: 'from route' },
-  });
-};
-
-const createTour = (req, res) => {
-  res.status(201).json({ status: 'success', data: { tour: req.body } });
-};
-const getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    idParam: req.params.id,
-    data: { tour: req.body },
-  });
-};
-
-const updateTour = (req, res) => {
-  const { id, name, description } = req.body;
-  res.status(200).json({ status: 'success', data: { id, name, description } });
-};
-
-const deleteTour = (req, res) => {
-  const { id } = req.params;
-  res.status(204).json({ status: 'success', data: null, id: id });
-};
 /*******************routes**************************/
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.use('/api/v1/tours', tourRouter);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
 /******************server********************************/
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
