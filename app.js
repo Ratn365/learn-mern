cbe4b6478f9bc6d73a52a2c509bc8e18d75a0616
Ratn365 <Ratnesh.visionias@outlook.com>
2023-02-27 17:57:02 +0530

    2.1 Routes getAll and post

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+/*****************App imports****************************/
 const express = require('express');
 const app = express();
-/*****************App imports****************************/
-//routes
-app.get('/', (req, res) => res.send('Hello World!'));
+/*****************MiddleWare*****************************/
 
-//server
+app.use(express.json()); //for post request req.body
+
+/*******************routes**************************/
+app.get('/api/v1/tours', (req, res) => res.send('Hello World!'));
+app.post('/api/v1/tours', (req, res) => {
+  res.send('POST request to the homepage');
+});
+
+/******************server********************************/
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
