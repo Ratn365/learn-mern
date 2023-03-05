3ba3aa39fc2fc86a7a8b48970c40fb8a47895125
Ratn365 <Ratnesh.visionIas@outlook.com>
2023-03-05 18:27:35 +0530

    file system Crud init setup

--- server.js
+++ server.js
@@ -1,21 +1,6 @@
 const app = require('./app');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 /******************server********************************/
 dotenv.config({ path: './config.env' });
-const connectionString = process.env.CON_STRING.replace(
-  '<password>',
-  process.env.PASSWORD
-);
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then((con) => {
-    console.log('Db connection Succecss');
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
