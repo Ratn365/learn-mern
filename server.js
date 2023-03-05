d1a4f4b13c1318b95bc3c055a5a976d81184b5d7
Ratn365 <Ratnesh.visionIas@outlook.com>
2023-03-05 18:23:09 +0530

    6.1 mongoose and dot env configurations

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 /******************server********************************/
-const port = 3000;
+dotenv.config({ path: './config.env' });
+const connectionString = process.env.CON_STRING.replace(
+  '<password>',
+  process.env.PASSWORD
+);
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('Db connection Succecss');
+  });
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
